19cf8c867572400d707bc6fd9876cf9976162c79
Ellaine Dela Cruz <maellaineanis@gmail.com>
2022-05-21 10:33:40 +0800

    final

--- script.js
+++ script.js
@@ -1,3 +1,37 @@
+// const videoPlayer = document.querySelector('.video-player')
+// const video = videoPlayer.querySelector('.video')
+// const playButton = videoPlayer.querySelector('.play-button')
+// const pauseButton = videoPlayer.querySelector('.pause-button')
+
+// // playButton.addEventListener('click', (event) => {
+// //     if (video.pause) {
+// //         video.play()
+// //         event.target.textContent = '▌▌'
+// //     }
+
+// //     else {
+// //         video.pause()
+// //         event.target.textContent = '►'
+// //     }
+// // })
+
+// playButton.addEventListener("click", function() {
+//     if (video.pause) {
+//         video.play()
+//         playButton.style.display = 'none'
+//     }
+//   });
+
+// video.addEventListener('onmouseover', function () {
+//     if (video.pause) {
+//         playButton.style.display = 'block'
+//     }
+
+//     else {
+//         pauseButton.style.display = 'block'
+//     }
+// })
+
 
 // popup modal
 const myPopup = document.querySelector('#modal-section');
@@ -12,28 +46,26 @@ closeMyPopup.onclick = function () {
     myPopup.style.display='none';
 }
 
-// thank you page
-(() => {
-    const searchParams = new URLSearchParams(window.location.search)
-    const email = searchParams.get('email')
-    console.log(email)
-    const domName = document.querySelector('data-name')
-    console.log(domName)
-  
-    if(domName) {
-      domName.textContent = 'Yey! We sent an email to ' + email
+const header = document.querySelector('#header');
+const sticky = document.querySelector('.sticky').classList;
+
+$(window).scroll(function() {
+    if (this.scrollTop > 20) {
+        header.classList.add = ('sticky');
     }
-  })()
+    else {
+        header.classList.remove = ('sticky');
+    }
+})
 
-// sticky header
-// const header = document.querySelector('#header');
-// const sticky = document.querySelector('.sticky').classList;
+(() => {
+  const searchParams = new URLSearchParams(window.location.search)
+  const email = searchParams.get('email')
+  console.log(email)
+  const domName = document.querySelector('data-name')
+  console.log(domName)
 
-// $(document).scroll(function() {
-//     if (this.scrollTop > 100) {
-//         header.classList.add = ('sticky');
-//     }
-//     else {
-//         header.classList.remove = ('sticky');
-//     }
-// })
+  if(domName) {
+    domName.textContent = 'Yey! We sent an email to ' + email
+  }
+})()
